fa4b757bfbc16d58b31944903455552b91ce05f4
Mauricio Uhlig <mauriciojastrowuhlig@gmail.com>
2024-03-11 17:22:12 -0300

    Added middleware for contet type response on header

--- routers/router.go
+++ routers/router.go
@@ -4,16 +4,19 @@ import (
 	"net/http"
 
 	"github.com/MauricioUhlig/go-rest-api/controllers"
+	"github.com/MauricioUhlig/go-rest-api/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/personalidades", controllers.Home).Methods("Get")
 	r.HandleFunc("/personalidades/{id}", controllers.GetById).Methods("Get")
 	r.HandleFunc("/personalidades", controllers.CreateNewPersinalidade).Methods("Post")
 	r.HandleFunc("/personalidades/{id}", controllers.DeletePersonalidade).Methods("Delete")
 	r.HandleFunc("/personalidades/{id}", controllers.EditPersonalidade).Methods("Put")
 
-	http.ListenAndServe(":8000", r)
+	http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r))
 }
